d39354323024648bcdf609bee4c7546a621145bd
Conrado Einstein <conradoeinsteinpro@gmail.com>
2023-09-28 10:02:00 -0300

    ##

---
+++ src/test/java/com/br/eletra/services/MeterLineServiceTest.java
@@ -0,0 +1,11 @@
+package com.br.eletra.services;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter
+
+@ExtendWith(MockitoExtension.class)
+public class MeterLineServiceTest {
+
+
+
+}
